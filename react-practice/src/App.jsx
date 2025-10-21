bd2c087dc6cbcc852024f06ce4cd891f587300db
김민중 <f1minn2r@naver.com>
2025-10-21 17:31:15 +0900

    feat: App 컴포넌트 내 app 태그 Tailwind 클래스명 연결

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -2,7 +2,7 @@ import Example1_counter from "./01_useState/Example1_counter";
 
 function App() {
   return (
-    <div className="app">
+    <div className="app p-20 m-auto max-w-4xl">
       <Example1_counter />
     </div>
   );
