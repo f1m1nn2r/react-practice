5ab5b800fe1114c7fbee38f2bb9218f4647b779d
김민중 <f1minn2r@naver.com>
2025-10-21 17:30:23 +0900

    remove: Tailwind 사용으로 reset.css 제거

--- react-practice/src/main.jsx
+++ react-practice/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./reset.css";
+import "./components/main.css";
 
 createRoot(document.getElementById("root")).render(<App />);
