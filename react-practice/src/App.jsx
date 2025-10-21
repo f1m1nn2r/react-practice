15279044af5307bc47044f0d5927372dc03a8ef8
김민중 <f1minn2r@naver.com>
2025-10-21 22:56:45 +0900

    feat: 리액트 예제 1. useState를 이용한 투두리스트  예제 확인을 위한 컴포넌트 추가

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,11 +1,13 @@
 import Example1_counter from "./01_useState/Example1_counter";
 import Example2_input from "./01_useState/Example2_input";
+import Example3_todoList from "./01_useState/Example3_todoList";
 
 function App() {
   return (
     <div className="app p-20 m-auto max-w-4xl">
       <Example1_counter />
       <Example2_input />
+      <Example3_todoList />
     </div>
   );
 }
