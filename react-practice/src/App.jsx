753c20e59d8eea86b2d1f75987b3612145f8db39
김민중 <f1minn2r@naver.com>
2025-10-21 21:24:11 +0900

    feat: 리액트 예제 1. useState를 이용한 인풋값 실시간 반영  예제 확인을 위한 컴포넌트 추가

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,9 +1,11 @@
 import Example1_counter from "./01_useState/Example1_counter";
+import Example2_input from "./01_useState/Example2_input";
 
 function App() {
   return (
     <div className="app p-20 m-auto max-w-4xl">
       <Example1_counter />
+      <Example2_input />
     </div>
   );
 }
