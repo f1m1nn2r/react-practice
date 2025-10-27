b90e7d3fa0b395c280cd5fe1845e46b29a275dbd
김민중 <f1minn2r@naver.com>
2025-10-27 20:19:56 +0900

    feat: 리액트 예제 1. useState를 이용한 좋아요 버튼 토글 예제 확인을 위한 컴포넌트 추가

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,6 +1,7 @@
 import Example1_counter from "./01_useState/Example1_counter";
 import Example2_input from "./01_useState/Example2_input";
 import Example3_todoList from "./01_useState/Example3_todoList";
+import Example4_heart from "./01_useState/Example4_heart";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Example1_counter />
       <Example2_input />
       <Example3_todoList />
+      <Example4_heart />
     </div>
   );
 }
