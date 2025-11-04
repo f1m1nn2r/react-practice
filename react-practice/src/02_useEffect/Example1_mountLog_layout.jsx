746165ae503ec17cb4b806bf28b57ed69e363c52
김민중 <f1minn2r@naver.com>
2025-11-04 21:39:07 +0900

    Refactor: Example1(mountLog) 컴포넌트를 별도 디렉토리로 이동하여 정리

--- react-practice/src/02_useEffect/Example1_mountLog_layout.jsx
+++ react-practice/src/02_useEffect/Example1_mountLog_layout.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import Example1_mountLog from "./Example1_mountLog";
+import Example1_mountLog from "./mountLog/Example1_mountLog";
 
 export default function Example1_mountLog_layout() {
   const [showTimer, setShowTimer] = useState(true);
 
   return (
-    <>
+    <section className="mb-30">
+      <header className="mb-10">
+        <p className="text-2xl font-medium">
+          Example1. 컴포넌트 마운트 시 콘솔 출력 및<br />
+          타이머 만들기(setInterval 함수로 구현/cleanup 함수로 타이머 정리)
+        </p>
+      </header>
       <button
         className="block bg-blue-300 cursor-pointer p-2 mb-5"
         onClick={() => setShowTimer((prev) => !prev)}
@@ -13,6 +19,6 @@ export default function Example1_mountLog_layout() {
         타이머 숨기기
       </button>
       {showTimer ? <Example1_mountLog /> : <p>콘솔을 확인하세요</p>}
-    </>
+    </section>
   );
 }
