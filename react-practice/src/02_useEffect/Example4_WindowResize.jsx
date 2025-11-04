a61aad952f0179b3a4900357f6134a95774a2ece
김민중 <f1minn2r@naver.com>
2025-11-04 23:49:03 +0900

    feat: 창 크기 감지 이벤트 예제 파일 작업

---
+++ react-practice/src/02_useEffect/Example4_WindowResize.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function Example4_WindowResize() {
+  const [resizing, setResizing] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setResizing(window.innerWidth);
+    // console.log(resizing);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.addEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <section className="mb-30">
+      <header className="mb-10">
+        <p className="text-2xl font-medium">Example4. 창 크기 감지 이벤트</p>
+      </header>
+      <div>window.innerWidth: {resizing}</div>
+    </section>
+  );
+}
